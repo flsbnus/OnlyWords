2fe953c9ce8792ba059829f619e745f852d60642
flsbnus <81967132+flsbnus@users.noreply.github.com>
2023-02-17 17:34:44 +0900

    update

--- test1.py
+++ test1.py
@@ -1,12 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url="https://www.wordreference.com/enko/complicate"
+#https://www.wordreference.com/검색
+wordput=input()
+url=f"https://www.wordreference.com/enko/{wordput}"
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"}
 res=requests.get(url)
 bs=BeautifulSoup(res.content,'html.parser')
-mean=bs.select_one("#enko\:50944 > td.ToWrd")
-mean1=mean.find("em")
-mean.find("em").decompose()
-#<td class="ToWrd">~을 복잡하게 하다 <em class="POS2" data-lang="ko">동(타)</em></td>ㄴㄹ
-print(mean.text)
+mean=bs.select("td.ToWrd")
+print(mean)
+words=[]
+for i in range(len(mean)):
+    words.append(bs.select("td.ToWrd")[i].get_text())
+#print(words)
+search="한국어"
+for word in words:
+    if search in word:
+        words.remove(word)
+print(words)
+
